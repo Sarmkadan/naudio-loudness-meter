b9a4d30acdcb4a8795705d3853ea69157966b2b7
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 21:30:00 +0300

    Update SampleProviderLoudnessExtensions

---
+++ src/SampleProviderLoudnessExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleProvider
+{
+    public class LoudnessSampleProvider
+    {
+        public float GetLoudnessSample(float time)
+        {
+            return 0.5f; // placeholder value, replace with actual implementation
+        }
+    }
+}
